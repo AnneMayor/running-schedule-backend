816f9fc90c742855dd749aecb261f5039b7464be
annemayor <pranne1224@gmail.com>
2023-10-26 22:03:02 +0900

    feat: web crawling Util 구현

--- domain/src/main/kotlin/com/anne/domain/common/util/CrawlerUtil.kt
+++ domain/src/main/kotlin/com/anne/domain/common/util/CrawlerUtil.kt
@@ -2,9 +2,17 @@ package com.anne.domain.common.util
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 object CrawlerUtil {
-    fun parseHtmlToDto(url: String): Document {
+    fun getDocument(url: String): Document {
         return Jsoup.connect(url).get()
+    }
+    fun getHtmlBody(url: String): Element {
+        return Jsoup.connect(url).get().body()
+    }
+    fun selectElements(document: Document, key: String): Elements {
+        return document.select(key)
     }
 }
--- domain/src/test/kotlin/com/anne/domain/common/util/CrawlerUtilTest.kt
+++ domain/src/test/kotlin/com/anne/domain/common/util/CrawlerUtilTest.kt
@@ -6,19 +6,24 @@ import java.time.Instant
 
 class CrawlerUtilTest : ShouldSpec({
     context("CrawlerUtil") {
-        should("parseHtmlToDto") {
-            val url = "https://www.naver.com"
-            val document = CrawlerUtil.parseHtmlToDto(url)
-            document shouldNotBe null
-            document.body().allElements shouldNotBe null
-            println(document.body().allElements)
+        should("getHtlmBody") {
+            val url = "http://www.naver.com"
+            val body = CrawlerUtil.getHtmlBody(url)
+            body shouldNotBe null
+            body.allElements shouldNotBe null
+//            println(body.allElements)
         }
 
-//        should("기능 구현중") {
-//            val epoch = Instant.parse("2022-11-30T18:35:24.00Z").epochSecond
-//            val url = "http://www.roadrun.co.kr/schedule/list.php?today=$epoch&todays=Y"
-//            val document = CrawlerUtil.parseHtmlToDto(url)
-//            println(document.body())
-//        }
+        should("parseDocument") {
+            val epoch = Instant.parse("2023-10-10T18:35:24.00Z").epochSecond
+            val url = "http://www.roadrun.co.kr/schedule/list.php?today=$epoch&todays=Y"
+            val document = CrawlerUtil.getDocument(url)
+            val numOfRunningRace = document.select("font[color=red]").text()
+            document shouldNotBe null
+            document.allElements shouldNotBe null
+            numOfRunningRace shouldNotBe null
+//            println(document)
+            println(numOfRunningRace)
+        }
     }
 })
